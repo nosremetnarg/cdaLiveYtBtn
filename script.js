8be6f6abc467d672d0fc2db5fb5c68dc3e66e04e
Grant Emerson <grant.alex.emerson@gmail.com>
2022-03-04 13:18:15 -0600

    ready for demo

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// GET ELEMENT(S) TO ADJUST
+const watchLive = document.getElementById(watchLive);
+console.log(watchLive);
+
 /* CREATE NEW DATE OBJECT TO FIND WHAT DAY OF THE WEEK IT IS */
 const date = new Date()
 
@@ -9,18 +13,18 @@ console.log(dayOfWeek + ' day of the week')
 function checkForSunday(date) {
   if (date === 6) {
     console.log('Its Sunday')
-    
+    checkTimeOfDay(hour)
   } else {
     console.log('Its not Sunday')
   }
 }
 checkForSunday(dayOfWeek)
-const utc_offset = date.getTimezoneOffset()
 
-date.setMinutes(date.getMinutes() + utc_offset)
 
 // Convert Timezone
 // ET -5 UTC
+const utc_offset = date.getTimezoneOffset()
+date.setMinutes(date.getMinutes() + utc_offset)
 
 // Offsetting timezone from UTC
 let newYork_offset = -5 * 60
@@ -30,11 +34,22 @@ date.setMinutes(date.getMinutes() + newYork_offset)
 let hour = date.getHours();
 console.log(hour + ' hour');
 
+// CHECK THE TIME OF DAY
 function checkTimeOfDay(time) {
   if (time >= 9 && time <= 12) {
     console.log(' It"s Live Stream Time ');
+    // IF WITHIN THE TIME WINDOW CHANGE THE URL
+    updateURL();
   } else {
     console.log('Not livestream time');
+    // IF OUT OF THE TIMEZONE RESET THE URL
+    resetURL();
   }
 }
-checkTimeOfDay(hour)
+
+function updateURL() {
+  watchLive.setAttribute(href, 'https://www.youtube.com/c/LOLUMC')
+}
+function resetURL() {
+  watchLive.setAttribute(href, 'https://lolumc.org/media/')
+}
